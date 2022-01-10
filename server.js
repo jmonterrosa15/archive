73e096c8bc3c44024b462fc6f7d493b37146cf59
jmonterrosa15 <f.monterrosa3@gmail.com>
2022-01-10 12:24:59 -0500

    Deploying to heroku

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ const express = require('express');
 const app = express();
 
 //Tell the app object to host your static content from the dist folder
-app.use(express.static( __dirname + 'dist'));
+app.use(express.static( __dirname + '/dist/archive'));
 
 //...
 //Add your API endpoints here
 //You may have a folder called server, and have a separate.js file ofr each API endpoint
 //Register all your API endpoints, and in the handler use mongo or whatever to save the data
 
-app.get('/', (req, res)=>{
+/* app.get('/', (req, res)=>{
     res.status(200).send('node works!');
-})
+}); */
 
 //Here, you're catching all the invalid routes
 app.all('*', (req, res)=>{
-    res.status(200).sendFile( __dirname + '/dist/index.html');
-})
+    res.status(200).sendFile( __dirname + '/dist/archive/index.html');
+});
 
 app.listen(process.env.PORT || 8080, ()=>{
     console.log('App is listening to port 8080!')
