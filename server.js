35c4ebc550652505ec230945adfe8a7b4303d488
jmonterrosa15 <f.monterrosa3@gmail.com>
2022-01-10 12:32:58 -0500

    Another try

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.use(express.static( __dirname + '/dist/archive'));
 }); */
 
 //Here, you're catching all the invalid routes
+
+app.get('/', ()=>{
+    res.status(200).sendFile( __dirname + '/dist/archive/index.html');
+});
+
 app.all('*', (req, res)=>{
     res.status(200).sendFile( __dirname + '/dist/archive/index.html');
 });
