0df7c84e08914f10eaf964bbc59d63d259ccc025
jmonterrosa15 <f.monterrosa3@gmail.com>
2022-01-10 12:33:38 -0500

    Another try

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static( __dirname + '/dist/archive'));
 
 //Here, you're catching all the invalid routes
 
-app.get('/', ()=>{
+app.get('/', (req, res)=>{
     res.status(200).sendFile( __dirname + '/dist/archive/index.html');
 });
 
